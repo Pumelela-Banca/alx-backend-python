8710a070152b3219c3b66883f31719305b4e175c
Pumelela-Banca <pumelelabanca@gmail.com>
2024-01-11 19:19:56 +0200

    returns floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+function floor which takes a float n
+as argument and returns the floor of
+the float.
+"""
+import math
+
+
+def floor(n: float) -> int:
+    """
+    returns floor of float
+    """
+    return math.floor(n)
