c3690708badd367f00fc38635683cffe0ccce049
Pumelela-Banca <pumelelabanca@gmail.com>
2024-01-16 16:39:00 +0200

    counts time taken by functins using gather

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -1,4 +1,18 @@
 #!/usr/bin/env python3
 """
-
+measures time taken by 4 async functions
 """
+import asyncio
+import time
+
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    """
+    measures time
+    """
+    start = time.time()
+    await asyncio.gather(async_comprehension(), async_comprehension(),
+                         async_comprehension(), async_comprehension())
+    return time.time() - start
