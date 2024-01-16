69db8dcdefd4454adf3f156972e8ea5f4885d515
Pumelela-Banca <pumelelabanca@gmail.com>
2024-01-16 16:23:14 +0200

    removed test function

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -7,20 +7,10 @@ import random
 from typing import Iterator
 
 
-async def async_generator() -> Iterator[int]:
+async def async_generator() -> Iterator[float]:
     """
     yields float
     """
     for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
-
-if __name__ == "__main__":
-    async def print_yielded_values():
-        result = []
-        async for i in async_generator():
-            result.append(i)
-        print(result)
-
-
-    asyncio.run(print_yielded_values())
